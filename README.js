0b956543c3ca964dea0c8d9f37c9f6d1395025a4
saddzoe <73001138+saddzoe@users.noreply.github.com>
2021-01-31 07:46:03 -0500

    Update README.js

--- README.js
+++ README.js
@@ -1,3 +1,21 @@
 # ceasarsCipher
-This is a Caesars Cipher
-function
+// This is a Caesars Cipher
+function rot13(str) {
+  let answer = "";
+  let firstHalf = "abcdefghijklm".toUpperCase();
+  let lastHalf = "nopqrstuvwxyz".toUpperCase();
+
+  for (let i = 0; i < str.length; i += 1) {
+    let toDecode = str[i];
+
+    if (firstHalf.indexOf(toDecode) >= 0) {
+      answer += lastHalf[firstHalf.indexOf(toDecode)];
+    } else if (lastHalf.indexOf(toDecode) >= 0) {
+      answer += firstHalf[lastHalf.indexOf(toDecode)];
+    } else {
+      answer += toDecode;
+    }
+  }
+
+  return answer;
+}
